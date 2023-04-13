434085b80119ee8336934d68563f8485dd9788dc
pornoiya <49945883+pornoiya@users.noreply.github.com>
2023-04-13 22:21:30 +0500

    Update solution.py
    try 4

--- solution.py
+++ solution.py
@@ -4,11 +4,7 @@ from scipy.stats import mannwhitneyu
 
 chat_id = 146155552 # Ваш chat ID, не меняйте название переменной
 
-def solution(x: np.array) -> bool: # Одна или две выборке на входе, заполняется исходя из условия
-    # Измените код этой функции
-    # Это будет вашим решением
-    # Не меняйте название функции и её аргументы
+def solution(x: np.array, y: np.array) -> bool:
     alpha = 0.01
-  
     pval = mannwhitneyu(x, y, alternative="less").pvalue
     return pval < alpha
